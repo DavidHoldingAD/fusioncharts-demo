6235115fddcc04e4a62c0e5290e29d465189ed78
Valery Dachev <contact@vdachev.net>
2021-03-07 19:37:14 +0200

    Add a link to the "Home" page to the title.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
 	<div id="app">
-		<h1>FusionCharts for Vue</h1>
+		<h1>
+			FusionCharts for Vue
+		</h1>
 		<p>
+			<router-link :to="{ name: 'home' }">
+				Home
+			</router-link>
+			|
 			<router-link :to="{ name: 'plotting-live-line-chart-with-time-axis' }">
 				Plotting Live Line Chart with Time Axis
 			</router-link>
+			|
 			<router-link :to="{ name: 'plotting-multiple-series-on-time-axis' }">
 				Plotting Multiple Series on Time Axis
 			</router-link>
